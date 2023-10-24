a621266cf92f111e30e6963ee71570d4934c28c2
NagillaGanesh <143380666+NagillaGanesh@users.noreply.github.com>
2023-10-24 23:27:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxUDoxb93fUNnI0dgyo3lYTO-UlyqLtaXkAzKoG20uV6x-8z9-tyeExtTtOuGfIU1aHmQ'
+  const form = document.forms['submit-to-google-sheet']
+  const msg=document.getElementById('msg')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Thank you"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
 $(document).ready(function(){
 
     $(window).scroll(function(){
